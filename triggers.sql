6727b822a9e105b5726fdd79b789ca69d6d02386
Yatt0Ikigai <minaster2312@gmail.com>
2023-01-30 16:30:51 +0100

    Added Trigger
    as name says

---
+++ triggers.sql
@@ -0,0 +1,49 @@
+IF OBJECT_ID ('dbo.Opened_new_location') IS NOT NULL
+	DROP TRIGGER Opened_new_location;
+
+IF OBJECT_ID ('dbo.Make_Payment') IS NOT NULL
+	DROP TRIGGER Make_Payment;
+
+IF OBJECT_ID ('dbo.Register_fee') IS NOT NULL
+	DROP TRIGGER Register_fee;
+
+Go
+CREATE TRIGGER Opened_new_location ON Placowka 
+	AFTER INSERT
+	AS
+	BEGIN
+	 UPDATE Dolar
+	 Set Kurs = Kurs + 1
+END;
+
+Go
+CREATE TRIGGER Make_Payment ON Klienci
+	AFTER Update 
+	AS
+	BEGIN 
+		DECLARE @A int, @C int;
+
+		SELECT @A = ID_klienta, @C = CAST(A.Cena_Dynamiczna * B.Kurs as int) FROM Inserted
+			LEFT JOIN Oferta A ON ID_oferty = Id_biletu 
+			LEFT JOIN Dolar B ON ID_Dolara = 1 
+
+		INSERT INTO Przelew VALUES(@A,@C,getdate());
+	END
+
+GO
+
+
+Go
+CREATE TRIGGER Register_fee ON KLIENCI
+	AFTER INSERT 
+	AS
+	BEGIN 
+		DECLARE @A int, @C int;
+
+		SELECT @A = ID_klienta, @C = 5 * B.Kurs FROM Inserted
+			LEFT JOIN Dolar B ON ID_Dolara = 1 
+
+		INSERT INTO Przelew VALUES(@A,@C,getdate());
+	END
+
+Go
