d07c0c1067cc2213f8a6cb57a20bb0018a29c4bd
Yatt0Ikigai <minaster2312@gmail.com>
2023-01-30 17:04:05 +0100

    Tworzacy_sql
    Update dodano triggery + dolarek

--- tworzacy_plus_insert.sql
+++ tworzacy_plus_insert.sql
@@ -1,4 +1,5 @@
 
+
 IF OBJECT_ID (N'Bialka') IS NOT NULL
 DROP TABLE Bialka
 IF OBJECT_ID (N'Batony') IS NOT NULL
@@ -66,6 +67,7 @@ DROP TABLE Placowka;
 
 IF OBJECT_ID (N'Dolar') IS NOT NULL
 DROP TABLE Dolar;
+
 
 CREATE TABLE Dolar (
 	ID_Dolara int IDENTITY(1, 1) PRIMARY KEY,
@@ -1738,4 +1740,80 @@ VALUES (58, 'Guma Oporowa'),
 		SET @counter = @counter + 1;
 	END;
 
+
+INSERT INTO DOLAR VALUES (2);
+
+IF OBJECT_ID ('dbo.Opened_new_location') IS NOT NULL
+	DROP TRIGGER Opened_new_location;
+
+IF OBJECT_ID ('dbo.Make_Payment') IS NOT NULL
+	DROP TRIGGER Make_Payment;
+
+IF OBJECT_ID ('dbo.Register_fee') IS NOT NULL
+	DROP TRIGGER Register_fee;
+
+IF OBJECT_ID ('dbo.trgPodwyzkaSprzataczka') IS NOT NULL
+	DROP TRIGGER trgPodwyzkaSprzataczka;
+
+IF OBJECT_ID ('dbo.trgPodwyzkaOchroniarz') IS NOT NULL
+	DROP TRIGGER trgPodwyzkaOchroniarz;
+
+Go
+CREATE TRIGGER Opened_new_location ON Placowka 
+	AFTER INSERT
+	AS
+	BEGIN
+	 UPDATE Dolar
+	 Set Kurs = Kurs + 1
+END
+
+Go
+CREATE TRIGGER Make_Payment ON Klienci
+	AFTER Update 
+	AS
+	BEGIN 
+		DECLARE @A int, @C int;
+
+		SELECT @A = ID_klienta, @C = CAST(A.Cena_Dynamiczna * B.Kurs as int) FROM Inserted
+			LEFT JOIN Oferta A ON ID_oferty = Id_biletu 
+			LEFT JOIN Dolar B ON ID_Dolara = 1 
+
+		INSERT INTO Przelew VALUES(@A,@C,getdate());
+	END
+
+GO
+
+
+Go
+CREATE TRIGGER Register_fee ON KLIENCI
+	AFTER INSERT 
+	AS
+	BEGIN 
+		DECLARE @A int, @C int;
+
+		SELECT @A = ID_klienta, @C = 5 * B.Kurs FROM Inserted
+			LEFT JOIN Dolar B ON ID_Dolara = 1 
+
+		INSERT INTO Przelew VALUES(@A,@C,getdate());
+	END
+
+Go
+
+GO
+CREATE TRIGGER trgPodwyzkaSprzataczka ON Sprzataczka
+	AFTER UPDATE
+	AS
+	BEGIN
+		UPDATE Sprzataczka
+		SET Sprzataczka.Wyplata_Godzinowa = Sprzataczka.Wyplata_Godzinowa - 1
+	END
+
+GO
+CREATE TRIGGER trgPodwyzkaOchroniarz ON Ochrona
+	AFTER UPDATE
+	AS
+	BEGIN
+		UPDATE Ochrona
+		SET Ochrona.Wyplata_Godzinowa = Ochrona.Wyplata_Godzinowa - 1
+	END
 
