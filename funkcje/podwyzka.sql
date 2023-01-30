0ae8f3c9808ab2b8cba9f9e0cb0bee8bd5830764
michal157 <120804781+michal157@users.noreply.github.com>
2023-01-30 16:35:09 +0100

    Update podwyzka.sql

--- funkcje/podwyzka.sql
+++ funkcje/podwyzka.sql
@@ -5,14 +5,22 @@ DROP PROC podwyzka_ochrona
 
 CREATE PROC podwyzka_sprzataczka @id NVARCHAR(50), @nowaWyplata INT
 AS
+	IF (NOT EXISTS
+	(SELECT * FROM Sprzataczka WHERE Sprzataczka.ID_Pracownika = @id) OR
+	(SELECT Sprzataczka.Wyplata_Godzinowa FROM Sprzataczka WHERE Sprzataczka.ID_Pracownika = @id) > @nowaWyplata)
+		RETURN
 	UPDATE Sprzataczka
-	SET Sprzataczka.Wyplata_Godzinowa = @nowaWyplata
+	SET Sprzataczka.Wyplata_Godzinowa = @nowaWyplata + 1
 	WHERE Sprzataczka.ID_Pracownika = @id
 GO
 
 CREATE PROC podwyzka_ochrona @id NVARCHAR(50), @nowaWyplata INT
 AS
+	IF (NOT EXISTS
+	(SELECT * FROM Ochrona WHERE Ochrona.ID_Pracownika = @id) OR
+	(SELECT Ochrona.Wyplata_Godzinowa FROM Ochrona WHERE Ochrona.ID_Pracownika = @id) > @nowaWyplata)
+		RETURN
 	UPDATE Ochrona
-	SET Ochrona.Wyplata_Godzinowa = @nowaWyplata
+	SET Ochrona.Wyplata_Godzinowa = @nowaWyplata + 1
 	WHERE Ochrona.ID_Pracownika = @id
 GO
