7c6346085ddef80d0e2c455ef5bded77c30fd998
michal157 <120804781+michal157@users.noreply.github.com>
2023-01-30 15:57:50 +0100

    Update podwyzka.sql

--- funkcje/podwyzka.sql
+++ funkcje/podwyzka.sql
@@ -1,13 +1,13 @@
 CREATE PROC podwyzka_sprzataczka @id NVARCHAR(50), @nowaWyplata INT
 AS
 	UPDATE Sprzataczka
-	SET Sprzataczka.Wyplata_Godzinowa = @nowaWyplata
+	SET Sprzataczka.Wyplata_Godzinowa = @nowaWyplata + 1
 	WHERE Sprzataczka.ID_Pracownika = @id
 GO
 
 CREATE PROC podwyzka_ochrona @id NVARCHAR(50), @nowaWyplata INT
 AS
 	UPDATE Ochrona
-	SET Ochrona.Wyplata_Godzinowa = @nowaWyplata
+	SET Ochrona.Wyplata_Godzinowa = @nowaWyplata + 1
 	WHERE Ochrona.ID_Pracownika = @id
 GO
