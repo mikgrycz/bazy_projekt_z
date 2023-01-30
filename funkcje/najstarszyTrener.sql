4ff819306920922188d3818b0a6faf1529f05716
michal157 <120804781+michal157@users.noreply.github.com>
2023-01-30 16:47:54 +0100

    Create najstarszyTrener.sql

---
+++ funkcje/najstarszyTrener.sql
@@ -0,0 +1,13 @@
+CREATE FUNCTION dbo.najstarszyTrener()
+RETURNS int
+AS
+BEGIN
+RETURN 
+	(SELECT TOP 1 Pracownik.ID_Pracownika FROM Pracownik
+	WHERE Pracownik.DateOfBirth = (
+		SELECT MIN(Pracownik.DateOfBirth) FROM Trener JOIN Pracownik 
+		ON Trener.ID_Pracownika = Pracownik.ID_Pracownika
+		)
+	)
+END;
+GO
