ec1779d44df3ea0bec31a29195944486a9a7f6fb
michal157 <120804781+michal157@users.noreply.github.com>
2023-01-30 17:33:00 +0100

    Update dodaj_pracownika.sql

--- funkcje/procedury/dodaj_pracownika.sql
+++ funkcje/procedury/dodaj_pracownika.sql
@@ -6,7 +6,8 @@ IF OBJECT_ID(N'dodaj_sprzataczke') IS NOT NULL
 DROP PROC dodaj_sprzataczke
 IF OBJECT_ID(N'dodaj_ochroniarza') IS NOT NULL
 DROP PROC dodaj_ochroniarza
-CREATE PROCEDURE dbo.dodaj_pracownika @id int, @plac int, @imie nvarchar(255), @nazwisko nvarchar(255), @data int, @zawod nvarchar(255)
+
+CREATE PROCEDURE dbo.dodaj_pracownika @id INT, @plac INT, @imie NVARCHAR(255), @nazwisko NVARCHAR(255), @data INT, @zawod NVARCHAR(255)
 AS 
 	INSERT INTO
 	dbo.Pracownik VALUES  (@id, @plac, @imie, @nazwisko, @data, @zawod);
@@ -14,19 +15,19 @@ GO
 
  EXEC dbo.dodaj_pracownika @id = 1000, @plac = 2, @imie = 'Zbigniew', @nazwisko = 'Nowatorski', @data = 1969 , @zawod = 'Ochrona'
 
-CREATE PROCEDURE dbo.dodaj_trenera @ID_Pracownika int, @Specjalizacja  VARCHAR(10),	@CenaZaGodzine Int
+CREATE PROCEDURE dbo.dodaj_trenera @ID_Pracownika INT, @Specjalizacja  NVARCHAR(10),	@CenaZaGodzine INT
 AS 
 	INSERT INTO
 	dbo.Trener VALUES  (@ID_Pracownika, @Specjalizacja, @CenaZaGodzine);
 GO
 
-CREATE PROCEDURE dbo.dodaj_sprzataczke @ID_Pracownika int, @LiczbaGodzin  VARCHAR(10),	@CenaZaGodzine Int
+CREATE PROCEDURE dbo.dodaj_sprzataczke @ID_Pracownika INT, @LiczbaGodzin  NVARCHAR(10),	@CenaZaGodzine INT
 AS 
 	INSERT INTO
 	dbo.Sprzataczka VALUES  (@ID_Pracownika, @LiczbaGodzin, @CenaZaGodzine);
 GO
 
-CREATE PROCEDURE dbo.dodaj_Ochroniarza @ID_Pracownika int, @LiczbaGodzin  VARCHAR(10),	@CenaZaGodzine Int, @Srodki_Przymusu_Bezposredniego BIT,
+CREATE PROCEDURE dbo.dodaj_Ochroniarza @ID_Pracownika INT, @LiczbaGodzin  NVARCHAR(10),	@CenaZaGodzine INT, @Srodki_Przymusu_Bezposredniego BIT,
 				@Rozmiar_Biceps	Int
 AS 
 	INSERT INTO
